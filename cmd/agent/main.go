3bcaf8c7d80f7e05a6b37d5d6f5b4d496a2e876e
Daniel Howard <danielhoward314@gmail.com>
2025-04-08 09:27:02 -0400

    refactor cert mgr & pcap mgr setup into goroutines

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"time"
 
 	"github.com/danielhoward314/packet-sentry/agent"
 	psLog "github.com/danielhoward314/packet-sentry/internal/log"
@@ -20,37 +20,76 @@ func initializeAgent(psAgent *agent.Agent) error {
 	certManager := transport.NewCertificateManager(psAgent.Ctx, psAgent.BaseLogger, systemInfo)
 	pcapManager := psPCap.NewPCapManager(psAgent.Ctx, psAgent.BaseLogger)
 
-	// TODO: use goroutine and synchronization primitives instead of blocking
-	psAgent.BaseLogger.Info("ensuring client certificate is in place for mTLS")
-	err := certManager.HasValidCert()
-	if err != nil {
-		isRenewal := false
-		var certReqErr error
-		switch err.(type) {
-		case *transport.CertExpiringSoonError:
-			psAgent.BaseLogger.Warn("client certificate will expire within 30 days, requesting renewal")
-			isRenewal = true
-		default:
-			psAgent.BaseLogger.Warn("failed to find client certificate on disk, assuming this is first client cert request")
-			isRenewal = false
+	certCh := make(chan error, 1)
+	pcapCh := make(chan error, 1)
+	timeout := 2 * time.Minute
+
+	go func() {
+		psAgent.BaseLogger.Info("ensuring client certificate is in place for mTLS")
+		err := certManager.HasValidCert()
+		if err != nil {
+			isRenewal := false
+			switch err.(type) {
+			case *transport.CertExpiringSoonError:
+				psAgent.BaseLogger.Warn("client certificate will expire within 30 days, requesting renewal")
+				isRenewal = true
+			default:
+				psAgent.BaseLogger.Warn("failed to find client certificate on disk, assuming this is first client cert request")
+			}
+			err = certManager.RequestCert(isRenewal)
 		}
-		certReqErr = certManager.RequestCert(isRenewal)
-		if certReqErr != nil {
-			return certReqErr
+		certCh <- err
+	}()
+
+	go func() {
+		psAgent.BaseLogger.Info("ensuring pcap manager is ready")
+		err := pcapManager.EnsureReady()
+		if err != nil {
+			psAgent.BaseLogger.Error("failed pcap readiness check", psLog.KeyError, err)
 		}
+		pcapCh <- err
+	}()
+
+	timer := time.NewTimer(timeout)
+	defer timer.Stop()
+
+	var certErr, pcapErr error
+
+	select {
+	case certErr = <-certCh:
+		psAgent.BaseLogger.Info("certificate initialization completed", "error", certErr)
+	case <-timer.C:
+		return fmt.Errorf("timeout waiting for certificate initialization")
+	}
+
+	if certErr != nil {
+		return fmt.Errorf("certificate initialization failed: %w", certErr)
+	}
+
+	select {
+	case pcapErr = <-pcapCh:
+		psAgent.BaseLogger.Info("pcap initialization completed", "error", pcapErr)
+	case <-timer.C:
+		return fmt.Errorf("timeout waiting for pcap initialization")
+	}
+
+	if pcapErr != nil {
+		return fmt.Errorf("pcap initialization failed: %w", pcapErr)
 	}
 
 	psAgent.BaseLogger.Info("instantiating mTLS client")
 	mTLSClient, err := certManager.GetMTLSClient()
 	if err != nil {
 		psAgent.BaseLogger.Error("failed to get mTLS client", psLog.KeyError, err)
+		return err
 	}
 
 	psAgent.BaseLogger.Info("testing mTLS communication with server")
 	// TODO: env-based config of base URL
 	res, err := mTLSClient.Get("https://localhost:9443/mtls-test")
 	if err != nil {
-		log.Fatalf("mTLS request failed: %v", err)
+		psAgent.BaseLogger.Error("mTLS request failed", psLog.KeyError, err)
+		return err
 	}
 	defer res.Body.Close()
 	if res.StatusCode != http.StatusOK {
@@ -59,16 +98,14 @@ func initializeAgent(psAgent *agent.Agent) error {
 		return err
 	}
 
-	psAgent.BaseLogger.Info("ensuring pcap manager is ready")
-	err = pcapManager.EnsureReady()
-	if err != nil {
-		psAgent.BaseLogger.Error("failed pcap readiness check", psLog.KeyError, err)
-		return err
-	}
+	psAgent.BaseLogger.Info("mTLS client and server communication successful")
+
+	psAgent.BaseLogger.Info("all dependencies ready, proceeding with injection")
 	psAgent.InjectDependencies(
 		certManager,
 		mTLSClient,
 		pcapManager,
 	)
-	return err
+
+	return nil
 }
